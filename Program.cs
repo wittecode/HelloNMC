bc18bf5e65c0c8c1af2661791734dc1fc0d4a2eb
Hannah Witte <witteh@ad.nmc.edu>
2019-04-01 13:57:15 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloNMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+            // App: Hello NMC
+            // Author: Witte, Hannah
+            // Date: 4.1.19
+            //
+
+            Console.WriteLine("Hello NMC!");
+        }
+    }
+}
